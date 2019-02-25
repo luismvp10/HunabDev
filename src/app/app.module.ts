2a39efd1590c194b8fdebe2a88d23d62337592fb
luismvp10 <luismvalenp@gmail.com>
2019-02-25 00:11:34 -0600

    Adding fonts family and social networks section

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,21 @@ import { AppComponent } from './app.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { AngularFontAwesomeModule } from 'angular-font-awesome';
+import { ServiciosComponent } from './components/servicios/servicios.component';
+import { NosotrosComponent } from './components/nosotros/nosotros.component';
+import { ContactanosComponent } from './components/contactanos/contactanos.component';
+import { UneteComponent } from './components/unete/unete.component';
+import { PrivacidadComponent } from './components/privacidad/privacidad.component';
 @NgModule({
   declarations: [
     AppComponent,
     FooterComponent,
-    NavbarComponent
+    NavbarComponent,
+    ServiciosComponent,
+    NosotrosComponent,
+    ContactanosComponent,
+    UneteComponent,
+    PrivacidadComponent
   ],
   imports: [
     BrowserModule,
