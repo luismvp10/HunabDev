a6963b47f155573ebb510eda866684f9a0b1aeca
luismvp10 <luismvalenp@gmail.com>
2019-03-02 23:28:13 -0600

    last changes

--- src/app/components/nosotros/nosotros.component.ts
+++ src/app/components/nosotros/nosotros.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-nosotros',
   templateUrl: './nosotros.component.html',
-  styleUrls: ['./nosotros.component.css']
+  styleUrls: ['./nosotros.component.scss']
 })
 export class NosotrosComponent implements OnInit {
 
